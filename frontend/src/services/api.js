c5370d70a0382cdbeb9a24e2449c9c6ca4f61012
Sachin Sen <sachinsen1920@gmail.com>
2025-09-12 16:50:20 +0530

    add interceptors api setup usinge authStore

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,17 @@
 import axios from "axios";
+import useAuthStore from "../stores/authStore";
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
     withCredentials: true,
+});
+
+api.interceptors.response.use((config) => {
+    const { accessToken } = useAuthStore.getState();
+    if (accessToken) {
+        config.headers["Authorization"] = `Bearer ${accessToken}`;
+    }
+    return config;
 });
 
 export default api;
