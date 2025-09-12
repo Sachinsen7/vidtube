87995019f33d6d79c388593a4230445abb6e9670
Sachin Sen <sachinsen1920@gmail.com>
2025-09-12 17:04:22 +0530

    add more video apis endpoints

--- frontend/src/services/video.js
+++ frontend/src/services/video.js
@@ -16,3 +16,22 @@ export const publishVideo = async (formData) => {
     });
     return response.data.data;
 };
+
+export const updateVideo = async (videoId, formData) => {
+    const response = await api.patch(`/api/v1/videos/${videoId}`, formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+    });
+    return response.data.data;
+};
+
+export const deleteVideo = async (videoId) => {
+    const response = await api.delete(`/api/v1/videos/${videoId}`);
+    return response.data.data;
+};
+
+export const togglePublishStatus = async (videoId) => {
+    const response = await api.patch(
+        `/api/v1/videos/toggle/publish/${videoId}`
+    );
+    return response.data.data;
+};
