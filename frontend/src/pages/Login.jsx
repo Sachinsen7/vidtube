5a8bcb9c0666ab6191c00af07218b7c595cbc55b
Sachin Sen <sachinsen1920@gmail.com>
2025-09-21 00:59:13 +0530

    update login page fix width

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import { Box, Typography } from "@mui/material";
 import LoginForm from "../components/auth/loginForm";
 
 function Login() {
     return (
-        <div>
+        <Box
+            sx={{
+                width: "100%",
+                maxWidth: "100%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                minHeight: "60vh",
+            }}
+        >
+            <Typography
+                variant="h4"
+                gutterBottom
+                sx={{ color: "var(--primary-color)", textAlign: "center" }}
+            >
+                Welcome Back
+            </Typography>
             <LoginForm />
-        </div>
+        </Box>
     );
 }
 
