d11d120fc90f001d637c018f1f7162bef34ac00f
Sachin Sen <sachinsen1920@gmail.com>
2025-09-21 00:50:23 +0530

    add bookmark retweet feature

--- backend/src/controllers/tweet.controllers.js
+++ backend/src/controllers/tweet.controllers.js
@@ -4,21 +4,95 @@ import User from "../models/user.model.js";
 import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {
+    uploadOnCloudinary,
+    deleteFromCloudinary,
+} from "../utils/cloudinary.js";
 
 const createTweet = asyncHandler(async (req, res) => {
     const { content } = req.body;
     const userId = req.user?._id;
+    const imagePath = req.file?.path;
 
     if (!content) throw new ApiError(400, "Tweet content is required");
+
+    let image = "";
+    if (imagePath) {
+        const uploadedImage = await uploadOnCloudinary(imagePath);
+        if (!uploadedImage?.url)
+            throw new ApiError(500, "Failed to upload image");
+        image = uploadedImage.url;
+    }
 
     const tweet = await TweetModel.create({
         content,
         owner: userId,
+        image,
     });
 
     return res
         .status(201)
         .json(new ApiResponse(201, tweet, "Tweet created successfully"));
+});
+
+const retweetTweet = asyncHandler(async (req, res) => {
+    const { tweetId } = req.params;
+    const userId = req.user?._id;
+
+    if (!isValidObjectId(tweetId)) throw new ApiError(400, "Invalid tweet ID");
+
+    const tweet = await TweetModel.findById(tweetId);
+    if (!tweet) throw new ApiError(404, "Tweet not found");
+
+    const isRetweeted = tweet.retweets.includes(userId);
+    if (isRetweeted) {
+        tweet.retweets = tweet.retweets.filter(
+            (id) => id.toString() !== userId.toString()
+        );
+    } else {
+        tweet.retweets.push(userId);
+    }
+    await tweet.save();
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                tweet,
+                isRetweeted ? "Tweet unretweeted" : "Tweet retweeted"
+            )
+        );
+});
+
+const bookmarkTweet = asyncHandler(async (req, res) => {
+    const { tweetId } = req.params;
+    const userId = req.user?._id;
+
+    if (!isValidObjectId(tweetId)) throw new ApiError(400, "Invalid tweet ID");
+
+    const tweet = await TweetModel.findById(tweetId);
+    if (!tweet) throw new ApiError(404, "Tweet not found");
+
+    const isBookmarked = tweet.bookmarks.includes(userId);
+    if (isBookmarked) {
+        tweet.bookmarks = tweet.bookmarks.filter(
+            (id) => id.toString() !== userId.toString()
+        );
+    } else {
+        tweet.bookmarks.push(userId);
+    }
+    await tweet.save();
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                tweet,
+                isBookmarked ? "Tweet unbookmarked" : "Tweet bookmarked"
+            )
+        );
 });
 
 const getUserTweets = asyncHandler(async (req, res) => {
@@ -27,12 +101,11 @@ const getUserTweets = asyncHandler(async (req, res) => {
     if (!isValidObjectId(userId)) throw new ApiError(400, "Invalid user ID");
 
     const user = await User.findById(userId);
-
     if (!user) throw new ApiError(404, "User not found");
 
-    const tweets = await TweetModel.find({ owner: userId }).sort({
-        createdAt: -1,
-    });
+    const tweets = await TweetModel.find({ owner: userId })
+        .populate("owner", "username fullName avatar")
+        .sort({ createdAt: -1 });
 
     return res
         .status(200)
@@ -75,6 +148,9 @@ const deleteTweet = asyncHandler(async (req, res) => {
         throw new ApiError(403, "You are not authorized to delete this tweet");
     }
 
+    if (tweet.image) {
+        await deleteFromCloudinary(tweet.image);
+    }
     await tweet.deleteOne();
 
     return res
@@ -82,4 +158,11 @@ const deleteTweet = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, {}, "Tweet deleted successfully"));
 });
 
-export { createTweet, getUserTweets, updateTweet, deleteTweet };
+export {
+    createTweet,
+    getUserTweets,
+    updateTweet,
+    deleteTweet,
+    retweetTweet,
+    bookmarkTweet,
+};
