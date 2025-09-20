56b7600a9aae8fe997d87f07163968a21b14b121
Sachin Sen <sachinsen1920@gmail.com>
2025-09-21 00:59:43 +0530

    fix width of subscription page

--- frontend/src/pages/Subscriptions.jsx
+++ frontend/src/pages/Subscriptions.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { Box, Typography, useMediaQuery } from "@mui/material";
+import SubscriptionFeed from "../components/subscription/SusbscriptionFeed";
+import { useAuth } from "../hooks/useAuth";
 
-function Subscriptions() {
-    return <div>Subscriptions</div>;
-}
+const Subscriptions = () => {
+    const { user } = useAuth();
+    const isMobile = useMediaQuery("(max-width:600px)");
+
+    return (
+        <Box
+            sx={{
+                p: { xs: 2, sm: 3, md: 4 },
+                width: "100%",
+                maxWidth: "100%",
+            }}
+        >
+            <Typography
+                variant={isMobile ? "h5" : "h4"}
+                gutterBottom
+                sx={{
+                    color: "var(--primary-color)",
+                    textAlign: { xs: "center", sm: "left" },
+                }}
+            >
+                {user
+                    ? "Your Subscriptions"
+                    : "Please log in to view subscriptions"}
+            </Typography>
+            {user && <SubscriptionFeed channelId={user._id} />}
+        </Box>
+    );
+};
 
 export default Subscriptions;
