a851739637d916b120240543148d209b96afaf8e
Sachin Sen <sachinsen1920@gmail.com>
2025-09-21 00:51:49 +0530

    add bookmark retweet and more

--- backend/src/routes/tweet.routes.js
+++ backend/src/routes/tweet.routes.js
@@ -4,14 +4,22 @@ import {
     deleteTweet,
     getUserTweets,
     updateTweet,
+    retweetTweet,
+    bookmarkTweet,
 } from "../controllers/tweet.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middlewares.js";
 
 const router = Router();
 router.use(verifyJWT);
 
-router.route("/tweet").post(createTweet);
+router.route("/").post(verifyJWT, upload.single("image"), createTweet);
 router.route("/user/:userId").get(getUserTweets);
-router.route("/:tweetId").patch(updateTweet).delete(deleteTweet);
+router
+    .route("/:tweetId")
+    .patch(verifyJWT, updateTweet)
+    .delete(verifyJWT, deleteTweet);
+router.route("/retweet/:tweetId").post(verifyJWT, retweetTweet);
+router.route("/bookmark/:tweetId").post(verifyJWT, bookmarkTweet);
 
 export default router;
