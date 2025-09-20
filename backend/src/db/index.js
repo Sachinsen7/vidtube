a0ebaebbb0ffbf782379c6b77c38ad002aad2e2a
Sachin Sen <sachinsen1920@gmail.com>
2025-09-21 00:50:35 +0530

    update db connection debug

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -2,19 +2,18 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constant.js";
 
 const connectDatabase = async () => {
-  try {
-    const connectionDatabase = await mongoose.connect(
-      `${process.env.MONGODB_URL}`
-    );
+    try {
+        const connectionDatabase = await mongoose.connect(
+            `${process.env.MONGODB_URL}`
+        );
 
-    console.log(
-      `Mongoose Connection successful ! host ${connectionDatabase.connection.host}`
-    );
-  } catch {
-    (err) => {
-      console.log("Connetcion failed", err);
-    };
-  }
+        console.log(
+            `Mongoose Connection successful ! host ${connectionDatabase.connection.host}`
+        );
+    } catch (err) {
+        console.log("Connection failed", err);
+        throw err;
+    }
 };
 
 export default connectDatabase;
