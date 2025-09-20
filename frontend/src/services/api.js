469bdb364b6021f896190e250061fd2c69a4e5e4
Sachin Sen <sachinsen1920@gmail.com>
2025-09-21 01:01:03 +0530

    update api endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,14 +4,27 @@ import useAuthStore from "../stores/authStore";
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
     withCredentials: true,
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
-api.interceptors.response.use((config) => {
+api.interceptors.request.use((config) => {
     const { accessToken } = useAuthStore.getState();
     if (accessToken) {
-        config.headers["Authorization"] = `Bearer ${accessToken}`;
+        config.headers.Authorization = `Bearer ${accessToken}`;
     }
     return config;
 });
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            useAuthStore.getState().logout();
+        }
+        return Promise.reject(error);
+    }
+);
 
 export default api;
