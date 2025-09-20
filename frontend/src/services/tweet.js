55b7a9eda767d744c35d920ee691d2ed3a54e508
Sachin Sen <sachinsen1920@gmail.com>
2025-09-21 01:01:30 +0530

    update tweet service

--- frontend/src/services/tweet.js
+++ frontend/src/services/tweet.js
@@ -1,12 +1,16 @@
 import api from "./api";
 
-export const createTweet = async (content) => {
-    const response = await api.post("/api/v1/tweets/tweet", { content });
+export const getUserTweets = async (userId, { page, limit }) => {
+    const response = await api.get(
+        `/api/v1/tweets/user/${userId}?page=${page}&limit=${limit}`
+    );
     return response.data.data;
 };
 
-export const getUserTweets = async (userId) => {
-    const response = await api.get(`/api/v1/tweets/user/${userId}`);
+export const createTweet = async (formData) => {
+    const response = await api.post("/api/v1/tweets", formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+    });
     return response.data.data;
 };
 
@@ -16,6 +20,15 @@ export const updateTweet = async (tweetId, content) => {
 };
 
 export const deleteTweet = async (tweetId) => {
-    const response = await api.delete(`/api/v1/tweets/${tweetId}`);
+    await api.delete(`/api/v1/tweets/${tweetId}`);
+};
+
+export const retweetTweet = async (tweetId) => {
+    const response = await api.post(`/api/v1/tweets/retweet/${tweetId}`);
+    return response.data.data;
+};
+
+export const bookmarkTweet = async (tweetId) => {
+    const response = await api.post(`/api/v1/tweets/bookmark/${tweetId}`);
     return response.data.data;
 };
