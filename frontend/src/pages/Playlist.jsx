50ab28e3511fea8a91b07a70c167fcda5339ebe4
Sachin Sen <sachinsen1920@gmail.com>
2025-09-21 00:59:26 +0530

    fix width of playlist page

--- frontend/src/pages/Playlist.jsx
+++ frontend/src/pages/Playlist.jsx
@@ -35,11 +35,8 @@ const Playlist = () => {
     return (
         <Box
             sx={{
-                backgroundColor: "var(--background-color)",
-                minHeight: "100vh",
-                p: 2,
-                maxWidth: 1200,
-                mx: "auto",
+                width: "100%",
+                maxWidth: "100%",
             }}
         >
             <Typography
