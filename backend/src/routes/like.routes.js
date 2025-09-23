bc20050f436ad0634af735441bdffdd3b6619f4c
Sachin Sen <sachinsen1920@gmail.com>
2025-09-24 04:31:54 +0530

    updatre like routes

--- backend/src/routes/like.routes.js
+++ backend/src/routes/like.routes.js
@@ -4,12 +4,21 @@ import {
     toggleCommentLike,
     toggleVideoLike,
     toggleTweetLike,
+    likeVideo,
+    unlikeVideo,
+    likeComment,
+    unlikeComment,
+    likeTweet,
+    unlikeTweet,
 } from "../controllers/like.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
-router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+router.use(verifyJWT);
 
+router.route("/video/:videoId").post(likeVideo).delete(unlikeVideo);
+router.route("/comment/:commentId").post(likeComment).delete(unlikeComment);
+router.route("/tweet/:tweetId").post(likeTweet).delete(unlikeTweet);
 router.route("/toggle/v/:videoId").post(toggleVideoLike);
 router.route("/toggle/c/:commentId").post(toggleCommentLike);
 router.route("/toggle/t/:tweetId").post(toggleTweetLike);
