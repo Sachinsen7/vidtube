4f68a304648cfa9e0ab996304aa312b7b21fa31a
Sachin Sen <sachinsen1920@gmail.com>
2025-09-24 04:30:17 +0530

    upadet like controller

--- backend/src/controllers/like.controllers.js
+++ backend/src/controllers/like.controllers.js
@@ -7,6 +7,116 @@ import VideoModel from "../models/video.model.js";
 import TweetModel from "../models/tweet.model.js";
 import commentModel from "../models/comment.model.js";
 
+const likeVideo = asyncHandler(async (req, res) => {
+    const { videoId } = req.params;
+    const userId = req.user?._id;
+
+    if (!mongoose.isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video id");
+    }
+    const video = await VideoModel.findById(videoId).lean();
+    if (!video) throw new ApiError(404, "Video not found");
+
+    await likeModel.updateOne(
+        { video: videoId, likedby: userId },
+        { $setOnInsert: { video: videoId, likedby: userId } },
+        { upsert: true }
+    );
+    const likesCount = await likeModel.countDocuments({ video: videoId });
+    return res
+        .status(200)
+        .json(new ApiResponse(200, { liked: true, likesCount }, "Video liked"));
+});
+
+const unlikeVideo = asyncHandler(async (req, res) => {
+    const { videoId } = req.params;
+    const userId = req.user?._id;
+    if (!mongoose.isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video id");
+    }
+    await likeModel.deleteOne({ video: videoId, likedby: userId });
+    const likesCount = await likeModel.countDocuments({ video: videoId });
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, { liked: false, likesCount }, "Video unliked")
+        );
+});
+
+const likeComment = asyncHandler(async (req, res) => {
+    const { commentId } = req.params;
+    const userId = req.user?._id;
+    if (!mongoose.isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid comment ID");
+    }
+    const comment = await commentModel.findById(commentId).lean();
+    if (!comment) throw new ApiError(404, "Comment not found");
+    await likeModel.updateOne(
+        { comment: commentId, likedby: userId },
+        { $setOnInsert: { comment: commentId, likedby: userId } },
+        { upsert: true }
+    );
+    const likesCount = await likeModel.countDocuments({ comment: commentId });
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, { liked: true, likesCount }, "Comment liked")
+        );
+});
+
+const unlikeComment = asyncHandler(async (req, res) => {
+    const { commentId } = req.params;
+    const userId = req.user?._id;
+    if (!mongoose.isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid comment ID");
+    }
+    await likeModel.deleteOne({ comment: commentId, likedby: userId });
+    const likesCount = await likeModel.countDocuments({ comment: commentId });
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                { liked: false, likesCount },
+                "Comment unliked"
+            )
+        );
+});
+
+const likeTweet = asyncHandler(async (req, res) => {
+    const { tweetId } = req.params;
+    const userId = req.user?._id;
+    if (!mongoose.isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid tweet ID");
+    }
+    const tweet = await TweetModel.findById(tweetId).lean();
+    if (!tweet) throw new ApiError(404, "Tweet not found");
+    await likeModel.updateOne(
+        { tweet: tweetId, likedby: userId },
+        { $setOnInsert: { tweet: tweetId, likedby: userId } },
+        { upsert: true }
+    );
+    const likesCount = await likeModel.countDocuments({ tweet: tweetId });
+    return res
+        .status(200)
+        .json(new ApiResponse(200, { liked: true, likesCount }, "Tweet liked"));
+});
+
+const unlikeTweet = asyncHandler(async (req, res) => {
+    const { tweetId } = req.params;
+    const userId = req.user?._id;
+    if (!mongoose.isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid tweet ID");
+    }
+    await likeModel.deleteOne({ tweet: tweetId, likedby: userId });
+    const likesCount = await likeModel.countDocuments({ tweet: tweetId });
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, { liked: false, likesCount }, "Tweet unliked")
+        );
+});
+
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const { videoId } = req.params;
 
@@ -113,4 +223,15 @@ const getLikedVideos = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, likes, "Liked videos fetched successfully"));
 });
 
-export { toggleCommentLike, toggleTweetLike, toggleVideoLike, getLikedVideos };
+export {
+    likeVideo,
+    unlikeVideo,
+    likeComment,
+    unlikeComment,
+    likeTweet,
+    unlikeTweet,
+    toggleCommentLike,
+    toggleTweetLike,
+    toggleVideoLike,
+    getLikedVideos,
+};
