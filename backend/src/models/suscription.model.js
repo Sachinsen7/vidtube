4c3c09c559089e0f9907e063db4bbeb8ed981397
Sachin Sen <sachinsen1920@gmail.com>
2025-09-24 04:31:15 +0530

    update subscription model

--- backend/src/models/suscription.model.js
+++ backend/src/models/suscription.model.js
@@ -15,4 +15,7 @@ const subscriptionSchema = new mongoose.Schema(
 );
 
 const Subscription = mongoose.model("Subscription", subscriptionSchema);
+
+subscriptionSchema.index({ subscriber: 1, channel: 1 }, { unique: true });
+subscriptionSchema.index({ channel: 1, createdAt: -1 });
 export default Subscription;
