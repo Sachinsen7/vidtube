d5c3badac962a35e2c555f8776babc8445046c6d
Sachin Sen <sachinsen1920@gmail.com>
2025-09-24 04:31:04 +0530

    update like model

--- backend/src/models/like.model.js
+++ backend/src/models/like.model.js
@@ -23,4 +23,8 @@ const likeSchema = new mongoose.Schema(
     { timestamps: true }
 );
 
+likeSchema.index({ likedby: 1, video: 1 }, { unique: true, sparse: true });
+likeSchema.index({ likedby: 1, comment: 1 }, { unique: true, sparse: true });
+likeSchema.index({ likedby: 1, tweet: 1 }, { unique: true, sparse: true });
+
 export default mongoose.model("Like", likeSchema);
